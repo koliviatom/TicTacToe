88d3b044cc9cbb36865a3f1e6fa3231058ce08cd
Olivia <koliviatom@gmail.com>
2019-06-09 09:41:39 -0400

    Add GamePlay

---
+++ src/sample/GamePlay.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class GamePlay {
+    private final Player p1;
+    private final Player p2;
+    private Board gameboard;
+
+    public GamePlay(Player p1, Player p2, Board board) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.gameboard = board;
+    }
+
+
+}
