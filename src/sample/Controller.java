367c9aded161351c5ee7cbf8c16acc876f7588e2
Olivia <koliviatom@gmail.com>
2019-06-09 20:22:11 -0400

    Add button action listeners

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
-    
+
+    public void press00(ActionEvent actionEvent) {
+    }
+
+    public void press01(ActionEvent actionEvent) {
+    }
+    public void press02(ActionEvent actionEvent) {
+    }
+    public void press10(ActionEvent actionEvent) {
+    }
+    public void press11(ActionEvent actionEvent) {
+    }
+    public void press12(ActionEvent actionEvent) {
+    }
+    public void press20(ActionEvent actionEvent) {
+    }
+    public void press21(ActionEvent actionEvent) {
+    }
+    public void press22(ActionEvent actionEvent) {
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,20 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("sample/gameView.fxml"));
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+
     }
 
 
