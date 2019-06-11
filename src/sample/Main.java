1a56eb2f2e69217a30ef878af0c5fc8018a0c42b
Olivia <koliviatom@gmail.com>
2019-06-10 22:02:26 -0400

    Fix FXML path

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("sample/gameView.fxml"));
+        GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("gameView.fxml"));
         primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
