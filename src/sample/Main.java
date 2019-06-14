dad9dd59e89bc0a9490936e99fb5ff7e86717de2
Olivia <koliviatom@gmail.com>
2019-06-13 22:39:04 -0400

    Add startup menu prototype in sample.fxml
    Added menu to select player names and game type

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,38 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
+        Platform.setImplicitExit(false);
+
+        Stage pickPlayers = new Stage();
+        GridPane popup = (GridPane)FXMLLoader.load(getClass().getResource("Popup.fxml"));
+        pickPlayers.setTitle("Tic Tac Toe");
+        pickPlayers.setScene(new Scene(popup, 600, 200));
+
         GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("gameView.fxml"));
         primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
 
+        pickPlayers.setOnHidden(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+
+                primaryStage.show();
+            }
+        });
+        pickPlayers.show();
     }
 
 
